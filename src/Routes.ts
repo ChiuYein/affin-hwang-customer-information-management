ad4c8468dd6939c0a1e41cab66352cff57a1740c
Chiu Yein <chiuyein@yahoo.com>
2022-11-15 21:43:28 +0800

    Create user service and routes

--- src/Routes.ts
+++ src/Routes.ts
@@ -1,15 +1,16 @@
 // Core
 import express from "express";
 
-const Apis = express.Router();
+// Information Management
+import { UserDetailInterface, UserService } from "@services/user.services";
 
-const users = [];
+const Apis = express.Router();
 
 /**
  * @swagger
- * /user:
+ * /register:
  *  post:
- *      summary: adds in a new user
+ *      summary: register new user
  *      requestBody:
  *          description: object of the body
  *          content:
@@ -23,22 +24,117 @@ const users = [];
  *                              type: string
  *                          emailAddress:
  *                              type: string
- *                          id:
- *                              type: number
  *      responses:
  *          200:
- *              description: adds in a new user into the DB
+ *              description: register new user
  */
-Apis.post("/user", (req, res) => {
-  users.push({
-    id: req?.body?.id,
-    firstName: req?.body?.firstName,
-    lastName:  req?.body?.lastName,
-    emailAddress: req?.body?.emailAddress
-  });
-  res.status(200).send({
-    data: users,
-  });
+Apis.post("/register", async(req, res) => {
+  try {
+    const userService = new UserService();
+    const userDetails: UserDetailInterface = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailAddress: req.body.emailAddress
+    }
+    const newUser = await userService.createNewUser(userDetails)
+    res.status(200).send({
+      data: newUser
+    })
+  } catch (error) {
+    console.error(error)
+  }
+});
+
+/**
+ * @swagger
+ * /user-details/{userId}:
+ *  get:
+ *      summary: Gets the details of the user by id
+ *      parameters:
+ *          - name: userId
+ *            in: path
+ *      responses:
+ *          200:
+ *              description: Gets the details of the user by id
+ */
+Apis.get("/user-details/:userId", async(req, res) => {
+  try {
+    const { userId } = req.params
+    const userService = new UserService();
+    const userDetails = await userService.getUserDetails(userId);
+    res.status(200).send({
+      data: userDetails
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+});
+
+/**
+ * @swagger
+ * /update/user-details:
+ *  put:
+ *      summary: update the user details
+ *      requestBody:
+ *          description: object of the body
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          id:
+ *                              type: string
+ *                          firstName:
+ *                              type: string
+ *                          lastName:
+ *                              type: string
+ *                          emailAddress:
+ *                              type: string
+ *      responses:
+ *          200:
+ *              description: update the user details
+ */
+Apis.put("/update/user-details", async(req, res) => {
+  try {
+    const userService = new UserService();
+    const userDetails: UserDetailInterface = {
+      id: req.body.id,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailAddress: req.body.emailAddress
+    }
+    const newUser = await userService.updateUserDetails(userDetails)
+    res.status(200).send({
+      data: newUser
+    })
+  } catch (error) {
+    console.error(error)
+  }
+});
+
+/**
+ * @swagger
+ * /delete/user-details/{userId}:
+ *  delete:
+ *      summary: delete the user by id
+ *      parameters:
+ *          - name: userId
+ *            in: path
+ *      responses:
+ *          200:
+ *              description: delete the user by id
+ */
+Apis.delete("/delete/user-details/:userId", async(req, res) => {
+  try {
+    const { userId } = req.params;  
+    const userService = new UserService();
+    const deletedUser = await userService.deleteUserById(userId)
+    res.status(200).send({
+      data: deletedUser
+    })
+  } catch (error) {
+    console.error(error)
+  }
 });
 
 export default Apis;
---
+++ src/services/user.services.ts
@@ -0,0 +1,50 @@
+// Information Management
+import Users from "@models/Users.model";
+
+export interface UserDetailInterface {
+    id?: number;
+    firstName: string;
+    lastName: string;
+    emailAddress: string;
+}
+
+export class UserService {
+   async createNewUser(userDetails: UserDetailInterface): Promise<UserDetailInterface> {
+    try {
+    const newUser = await Users .create({
+      firstName: userDetails.firstName,
+      lastName:  userDetails.lastName,
+      emailAddress: userDetails.emailAddress
+    })    
+    return newUser;
+    } catch (error) {
+        throw new Error(error)
+    }
+   }
+
+   async getUserDetails(userId: number): Promise<Users> {
+    try {
+        const userDetails = await Users.findByPk(userId);
+        return userDetails;
+    } catch (error) {
+        throw new Error(error)
+    }
+   }
+
+   async updateUserDetails(userDetails: UserDetailInterface): Promise<UserDetailInterface> {
+    try {
+        await Users.update( { firstName: userDetails.firstName, lastName: userDetails.lastName, emailAddress: userDetails.emailAddress}, {where: { id : userDetails.id }});
+        return userDetails;
+    } catch (error) {
+        throw new Error(error)
+    }
+   }
+
+   async deleteUserById(userId: number): Promise<number> {
+    try {
+        return await Users.destroy({ where: { id: userId } })
+    } catch (error) {
+        throw new Error(error)
+    }
+   }
+}
